05ca4f6bd17a816818ce1c974bdecfaef71d3ee2
Agustinaa01 <112518086+Agustinaa01@users.noreply.github.com>
2023-10-10 12:06:50 -0300

    orden bien tareas

--- assets/js/cargarTarea.js
+++ assets/js/cargarTarea.js
@@ -1,3 +1,5 @@
+// cargarTarea.js
+
 function cargarTarea() {
     $.ajax({
         url: '../assets/ajax/procesarDatosTarea.php',
@@ -7,14 +9,43 @@ function cargarTarea() {
             console.log("Respuesta del servidor:", data);
             if (data.success) {
                 var tableBody = document.getElementById("taskTableBody");
+
                 tableBody.innerHTML = '';
 
                 data.tareas.forEach(function (tarea) {
                     var newRow = tableBody.insertRow(tableBody.rows.length);
-                    newRow.insertCell(0).textContent = tarea.titulo; // Accede a 'titulo'
-                    newRow.insertCell(1).textContent = tarea.descripcion; // Accede a 'descripcion'
-                    newRow.insertCell(2).textContent = tarea.responsable; // Accede a 'responsable'
-                    newRow.insertCell(3).textContent = tarea.fecha_venc; // Accede a 'fecha_venc'
+                    newRow.insertCell(0).textContent = tarea.titulo;
+                    newRow.insertCell(1).textContent = tarea.descripcion;
+                    newRow.insertCell(2).textContent = tarea.responsable;
+
+                    // Agregar el "Vencimiento" en la columna de "Vencimiento"
+                    newRow.insertCell(3).textContent = tarea.fecha_venc;
+
+                    // Agregar un checkbox en la columna de "Terminado"
+                    var terminadoCell = newRow.insertCell(4);
+                    var terminadoCheckbox = document.createElement("input");
+                    terminadoCheckbox.type = "checkbox";
+                    terminadoCell.appendChild(terminadoCheckbox);
+
+                    // Agregar un dropdown para "Integrantes" en la columna de "Integrantes"
+                    var integrantesCell = newRow.insertCell(5);
+                    var selectIntegrantes = document.createElement("select");
+                    selectIntegrantes.id = "integrantes_" + tarea.id;
+
+                    // Agregar una opción por defecto
+                    var defaultOption = document.createElement("option");
+                    defaultOption.value = "";
+                    defaultOption.text = "Seleccionar Integrante";
+                    selectIntegrantes.appendChild(defaultOption);
+
+                    integrantesCell.appendChild(selectIntegrantes);
+
+                    // Agregar un enlace "Editar" en la columna de "Editar"
+                    var editarCell = newRow.insertCell(6);
+                    var editarText = document.createElement("a");
+                    editarText.href = "#"; // Puedes configurar el enlace de edición aquí
+                    editarText.textContent = "Editar";
+                    editarCell.appendChild(editarText);
                 });
             } else {
                 console.log("Error: " + data.message);
